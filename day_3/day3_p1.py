b6a104dea925bfd9301122d18918f9fdd438cf45
Wenjia Knight <wenjia.knight@thedatashed.co.uk>
2023-07-15 10:29:52 +0100

    minor change

--- day_3/day3_p1.py
+++ day_3/day3_p1.py
@@ -1,15 +1,3 @@
-# def get_rucksacks(input):
-#     with open(input) as file:
-#         lines = file.readlines()
-#         rucksacks = [line.strip() for line in lines]
-#         return rucksacks
-    
-# def sep_comp(rucksack):
-#     num_items = int(len(rucksack)/2)
-#     rucksack_1 = rucksack[0:num_items]
-#     rucksack_2 = rucksack[num_items:]
-#     return rucksack_1, rucksack_2
-
 with open('input.txt') as file:
     lines = file.readlines()
     rucksacks = [line.strip() for line in lines]
