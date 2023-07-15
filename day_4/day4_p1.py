71223411fc17fad5b001f4f3efeb3236f630bf95
Wenjia Knight <wenjia.knight@thedatashed.co.uk>
2023-07-15 12:32:01 +0100

    completed day 4 part 1

---
+++ day_4/day4_p1.py
@@ -0,0 +1,40 @@
+
+def get_assignments(input:str)->list:
+    with open(input) as file:
+        lines = file.readlines()
+        assignments = [line.strip() for line in lines]
+        return assignments
+
+# for each assignment, get section 1 and section 2 as two lists.
+def get_sections(assignment:str) -> tuple:
+    section = assignment.split(',')
+    section_1 = section[0].split('-')
+    section_2 = section[1].split('-')
+    return section_1,section_2
+
+def is_in_range(set_1:set, set_2:set) -> int:
+    total_num = 0
+    if set_1.issubset(set_2) or set_2.issubset(set_1):
+        total_num += 1
+    return total_num
+
+assignments = get_assignments('input.txt')
+sum = 0
+for assignment in assignments:
+    sections = get_sections(assignment=assignment)
+    start_1 = int(sections[0][0])
+    print(start_1)
+    end_1 = int(sections[0][1])
+    print(end_1)
+    start_2 = int(sections[1][0])
+    print(start_2)
+    end_2 = int(sections[1][1])
+    print(end_2)
+    set_1 = set(range(start_1, end_1+1))
+    print(set_1)
+    set_2 = set(range(start_2, end_2+1))
+    print(set_2)
+    total = is_in_range(set_1=set_1, set_2=set_2)
+    sum += total
+
+print(sum)
