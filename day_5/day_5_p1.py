4a55546d5642d62f21ba8c5ba37d15ff83282c00
Wenjia Knight <wenjia.knight@thedatashed.co.uk>
2023-07-16 19:06:16 +0100

    start day 5

--- day_5/day_5_p1.py
+++ day_5/day_5_p1.py
@@ -1,39 +1,19 @@
-# Crates moved in each step - varying stacks of crates 
-# crates can only be moved one at a time 
-#after all steps , which crate will be on top of each stack?
-# Start pos
-#     [D]    
-# [N] [C]    
-# [Z] [M] [P]
-#  1   2   3 
+with open('input.txt') as file:
+    lines = file.readlines()
+    rucksacks = [line.strip() for line in lines]
 
-# [D]        
-# [N] [C]    
-# [Z] [M] [P]
-#  1   2   3 
+print(rucksacks)
+# def parse_instruction(instruction):
+#     words_list = instruction.split()
+#     numbers = words_list[1::2]
+#     a = int(numbers[0])
+#     b = int(numbers[1])
+#     c = int(numbers[2])
+#     return (a,b,c)
 
+# def move_crate(crate_stacks,instruction):
+#     #move 1 from 2 to 1#
+#     pass
 
-# move 1 from 2 to 1
-# move 3 from 1 to 3
-# move 2 from 2 to 1
-# move 1 from 1 to 2
-#         [Z]
-#         [N]
-#         [D]
-# [C] [M] [P]
-#  1   2   3
-
-def parse_instruction(instruction):
-    words_list = instruction.split()
-    numbers = words_list[1::2]
-    a = int(numbers[0])
-    b = int(numbers[1])
-    c = int(numbers[2])
-    return (a,b,c)
-
-def move_crate(crate_stacks,instruction):
-    #move 1 from 2 to 1#
-    pass
-
-def move_crates(crate_stacks,instructions):
-    pass 
+# def move_crates(crate_stacks,instructions):
+#     pass 
